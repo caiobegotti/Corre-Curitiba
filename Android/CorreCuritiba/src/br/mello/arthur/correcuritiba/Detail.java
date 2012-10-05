11b30fe1525e6883c2804854b0b9f014304ff2ba
Arthur Renato Mello <arthur.mello85@gmail.com>
2012-10-05 17:21:09 -0300

    Adding new features:
    - Display Events details;
    - Save Calendar data in a local file;

---
+++ Android/CorreCuritiba/src/br/mello/arthur/correcuritiba/Detail.java
@@ -0,0 +1,19 @@
+package br.mello.arthur.correcuritiba;
+
+public class Detail {
+	private String title;
+	private String detail;
+
+	public Detail(String title, String detail) {
+		this.title = title;
+		this.detail = detail;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+}
