e853eb0e7cb1a051078e836f717410518777d78d
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-12-26 11:53:31 -0200

    [Android] Refactor to use fragments
    This application has a classic list/detail layout which is well-suited for
    fragments. We created a fragment for the events list and another for the
    event details. If the screen is large such as in a tablet, display fragments
    side-by-side. If not, keep each fragment in its own activity.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ Android/CorreCuritiba/src/br/mello/arthur/correcuritiba/EventsFragment.java
@@ -0,0 +1,40 @@
+package br.mello.arthur.correcuritiba;
+
+import java.util.Arrays;
+
+import com.actionbarsherlock.app.SherlockListFragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+public class EventsFragment extends SherlockListFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.events_fragment, container, false);    	
+	}
+
+	public void setEvents(Event[] events) {
+		Arrays.sort(events);
+		setListAdapter(new EventAdapter(getActivity(), R.layout.event_list_item, events));
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		DetailFragment fragment = (DetailFragment)getFragmentManager().findFragmentById(R.id.detail_fragment);
+		if (fragment != null && fragment.isInLayout()) {
+			fragment.displayEvent((Event)l.getItemAtPosition(position));
+		} else {			
+			Intent intent = new Intent(getActivity(), DetailActivity.class);
+			intent.putExtra("event", ((Event)l.getItemAtPosition(position)));
+			startActivity(intent);
+		}	
+	}
+
+}
--- Android/CorreCuritiba/src/br/mello/arthur/correcuritiba/MainActivity.java
+++ Android/CorreCuritiba/src/br/mello/arthur/correcuritiba/MainActivity.java
@@ -1,40 +1,31 @@
 package br.mello.arthur.correcuritiba;
 
-import java.util.Arrays;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 
 
-public class MainActivity extends SherlockListActivity {
+public class MainActivity extends SherlockFragmentActivity {
 	public static Event[] events;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
         
         if (events != null) {
-        	Arrays.sort(events);
-        	setListAdapter(new EventAdapter(this, R.layout.event_list_item, events));
+        	EventsFragment fragment = (EventsFragment)getSupportFragmentManager().findFragmentById(R.id.events_fragment);
+        	if (fragment != null && fragment.isInLayout()) {
+        		fragment.setEvents(events);
+        	}
         } else {
         	Toast.makeText(this, R.string.download_error, Toast.LENGTH_LONG).show();
         }            
     }
     
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-    	Intent intent = new Intent(this, DisplayEvent.class);
-    	intent.putExtra("event", ((Event)l.getItemAtPosition(position)));
-    	startActivity(intent);
-    }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
